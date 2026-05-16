9d06b3316429c67afd76843564202678662a6a6e
Nem <65216593+DinhHaiNam@users.noreply.github.com>
2026-05-16 01:41:49 +0000

    fix

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/commands/bedwars/subcmds/sensitive/setup/SetUpgrade.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/commands/bedwars/subcmds/sensitive/setup/SetUpgrade.java
@@ -36,6 +36,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.Location;
 
 import java.util.ArrayList;
 import java.util.List;
