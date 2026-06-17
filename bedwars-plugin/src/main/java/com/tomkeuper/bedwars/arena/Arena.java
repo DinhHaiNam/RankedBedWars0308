3deeeded1faa711ebd41e35e19af17283f5170b3
Nem <65216593+DinhHaiNam@users.noreply.github.com>
2026-06-17 04:10:14 +0000

    nah nah nah

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
@@ -390,9 +390,6 @@ public class Arena implements IArena {
             }
         }
 
-        @Getter
-        private Arena arena;
-
         for (String type : Arrays.asList("Shop", "Upgrade")) {
             if (yml.get("npcs." + type) != null) {
                 for (String s : yml.getStringList("npcs." + type)) {
