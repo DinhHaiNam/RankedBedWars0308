dea766136ffad5a3a24fd264ace53df5da4f40fd
Nem <65216593+DinhHaiNam@users.noreply.github.com>
2026-06-17 07:45:44 +0000

    fix

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
@@ -842,8 +842,8 @@ public class Arena implements IArena {
             }
             for (ITeam t : getTeams()) {
                 if (!t.isShopSpawned()) continue;
-                nms.spawnShopHologram(getConfig().getArenaLoc("Team." + t.getName() + ".Upgrade"), (getMaxInTeam() > 1 ? Messages.NPC_NAME_TEAM_UPGRADES.replace("%group%", group) : Messages.NPC_NAME_SOLO_UPGRADES.replace("%group%", group)), Collections.singletonList(p), t);
-                nms.spawnShopHologram(getConfig().getArenaLoc("Team." + t.getName() + ".Shop"), (getMaxInTeam() > 1 ? Messages.NPC_NAME_TEAM_SHOP.replace("%group%", group) : Messages.NPC_NAME_SOLO_SHOP.replace("%group%", group)), Collections.singletonList(p), t);
+                nms.spawnShopHologram(getConfig().getArenaLoc("Team." + t.getName() + ".Upgrade"), (getMaxInTeam() > 1 ? Messages.NPC_NAME_TEAM_UPGRADES.replace("%group%", group) : Messages.NPC_NAME_SOLO_UPGRADES.replace("%group%", group)), Collections.singletonList(p), t.getArena());
+                nms.spawnShopHologram(getConfig().getArenaLoc("Team." + t.getName() + ".Shop"), (getMaxInTeam() > 1 ? Messages.NPC_NAME_TEAM_SHOP.replace("%group%", group) : Messages.NPC_NAME_SOLO_SHOP.replace("%group%", group)), Collections.singletonList(p), t.getArena());
                 for (IGenerator o : t.getGenerators()) {
                     o.updateHolograms(p);
                 }
